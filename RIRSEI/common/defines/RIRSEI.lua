e40b1a943eec6372581cda2e0d3c537f0f434b2a
NicolasGrosjean <grosjen@hotmail.fr>
2016-10-27 19:31:00 +0200

    Put the defines modifications in a separate file
    By this way, the mod is more modular (in other words more compatible
    with other mods)

---
+++ RIRSEI/common/defines/RIRSEI.lua
@@ -0,0 +1,15 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.DEMESNE_BASE_MAX_SIZE = 2.0
+NDefines.NDiplomacy.MAX_INFAMY_GAIN = 1
+
+NDefines.NCharacter.AGE_OF_MARRIAGE_MALE = 14
+NDefines.NCharacter.AGE_OF_MARRIAGE_FEMALE = 14
+
+NDefines.NReligion.CREATE_ANTIPOPE_PRESTIGE_COST = 5000
+
+NDefines.NMilitary.LEVY_MAINTENANCE_FACTOR = 1.4
+NDefines.NMilitary.RETINUE_INCREASE_PER_TECH = 3.0
+NDefines.NMilitary.RETINUE_HIRE_COST_MULTIPLIER = 0.08
+NDefines.NMilitary.RETINUE_REINFORCE_RATE = 0.05
+NDefines.NMilitary.RETINUE_CONSTANT_COST = 1.4
